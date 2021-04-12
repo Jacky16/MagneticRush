e835b2576147e225a21ea41e56e29a3b61c70d81
Venti <diguitys225@gmail.com>
2021-04-12 14:41:50 +0200

    ChargesManager + Añadido en inputmanager

---
+++ Assets/Scripts/ChargesManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargesManager : MonoBehaviour
+{
+    [SerializeField] GameObject positiveCharge;
+    [SerializeField] GameObject negativeCharge;
+    [SerializeField] int maxCharges;
+    int numCharges;
+    float selector;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        selector = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChargeSpawn(Vector3 posSpawn)
+    {
+        if(numCharges <= maxCharges)
+        {
+            if (selector == 1)
+            {
+                Instantiate(positiveCharge, posSpawn, Quaternion.identity, null);
+                numCharges++;
+            }
+            else if (selector == -1)
+            {
+                Instantiate(negativeCharge, posSpawn, Quaternion.identity, null);
+                numCharges++;
+            }
+        }
+        else
+        {
+            /*Poner mensaje en un canvas de: "Ya has colocado el máximo de partículas posibles
+             Si quieres colocar más, borra alguna de las actuales" (más cortito).*/
+        }
+    }
+
+    public void SetSelector(float _selector)
+    {
+        selector = _selector;
+    }
+}
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,25 +5,27 @@ using UnityEngine.InputSystem;
 public class InputManager : MonoBehaviour
 {
     Vector2 axisMouseWheel = Vector2.zero;
+    [SerializeField] ChargesManager charges;
     public void OnWheel(InputAction.CallbackContext ctx)
     {  
         if (ctx.performed)
         {
             axisMouseWheel = ctx.ReadValue<Vector2>();
+            charges.SetSelector(axisMouseWheel.y);
         }
     }
     public void OnPutCharge(InputAction.CallbackContext ctx)
     {
         if (ctx.started)
         {
-
+            
         }
     }
     public void OnDoForce(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
         {
-
+            
         }
     }
 }
