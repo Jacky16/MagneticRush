a98f109224b98d8fcc808c0e6a4cbf0e475916e4
Adria Martinez <adriamartinez94@gmail.com>
2021-04-12 14:42:45 +0200

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,6 +6,7 @@ public class InputManager : MonoBehaviour
 {
     Vector2 axisMouseWheel = Vector2.zero;
     [SerializeField] ChargesManager charges;
+    Vector2 mousePosition = Vector2.zero;
     public void OnWheel(InputAction.CallbackContext ctx)
     {  
         if (ctx.performed)
@@ -16,7 +17,7 @@ public class InputManager : MonoBehaviour
     }
     public void OnPutCharge(InputAction.CallbackContext ctx)
     {
-        if (ctx.started)
+        if (ctx.canceled)
         {
             
         }
@@ -26,6 +27,13 @@ public class InputManager : MonoBehaviour
         if (ctx.performed)
         {
             
+        }
+    }
+    public void OnMouseMovement(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+
         }
     }
 }
