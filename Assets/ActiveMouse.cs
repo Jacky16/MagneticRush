122daacf42cd2dc38dd176cdc21402a982c1a980
Adria Martinez <adriamartinez94@gmail.com>
2021-04-14 11:45:47 +0200

    Arreglado lo del raton

--- Assets/ActiveMouse.cs
+++ Assets/ActiveMouse.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ActiveMouse : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
         Cursor.visible = true;
     }
